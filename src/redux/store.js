1156b616fc73a58117a32319464b2f368e6b05d8
zsribna <zsribna@gmail.com>
2023-11-06 23:37:10 +0200

    delete

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './contactsReducer';
-
-export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-  },
-});
